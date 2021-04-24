16e370442e01d4a55d5e5c2d979cf29555ce9311
Philipp Toelke <pt@philipptoelke.de>
2021-04-24 16:22:59 +0000

    first ble ftp tests

---
+++ ble-ftp.py
@@ -0,0 +1,47 @@
+"""
+Demo the direct flying for the python interface
+
+Author: Amy McGovern
+"""
+
+from pyparrot.Minidrone import Mambo
+import sys
+
+import atexit
+
+# you will need to change this to the address of YOUR mambo
+mamboAddrs = ["d0:3a:2f:c6:e6:3b"]
+
+# make my mambo object
+# remember to set True/False for the wifi depending on if you are using the wifi or the BLE to connect
+mambos = [Mambo(x, use_wifi=False) for x in mamboAddrs]
+
+
+def callback(mambo):
+    print(mambo.sensors)
+
+
+for mambo in mambos:
+    print("trying to connect")
+    success = mambo.connect(num_retries=3)
+    print("connected: %s" % success)
+    if not success:
+        sys.exit(1)
+
+
+@atexit.register
+def land():
+    [x.land() for x in mambos]
+
+    print("disconnect")
+    [x.disconnect() for x in mambos]
+
+
+[x.set_user_sensor_callback(callback, x) for x in mambos]
+
+m = mambos[0]
+m.drone_connection._safe_ble_write(characteristic=m.drone_connection.ftp_characteristics['NORMAL_FTP_HANDLING'], packet=b'\x02\x01LIS/')
+
+import code
+
+code.interact(local=locals())
